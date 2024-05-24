db8f75349e7b0e3db95572cf1b548bdbb53cfda8
Hessie.K <138830912+HyeeunKim07@users.noreply.github.com>
2024-05-24 19:27:27 +0900

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,84 @@
+var map = L.map("map").setView([36.0, 127.5], 7);
+
+L.tileLayer("https://tiles.osm.kr/hot/{z}/{x}/{y}.png", {
+  maxZoom: 30,
+  attribution:
+    '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+// 커스텀 아이콘 생성
+var customIcon = L.icon({
+  iconUrl:
+    "https://postfiles.pstatic.net/MjAyNDA1MjRfODMg/MDAxNzE2NTQ2MzA2NjYx.6CGL9pdX-2uAmlUm7OrovnLyHkUMPJ1SfvaSmYgVGd8g.ahzAzMhbbkqUNvSfAAKzVIXF8ZDLZhQ3Fep2pvsXUssg.PNG/free-icon-location-pin-2776067.png?type=w966", // 아이콘 이미지 경로
+  iconSize: [50, 50], // 아이콘 크기
+  iconAnchor: [25, 50], // 앵커 지점
+  popupAnchor: [0, -50], // 팝업 위치 조정
+});
+
+// 적는 법 {latlng: [위도, 경도], popupContent: '이름'}
+var markers = [
+  {
+    latlng: [35.8032306, 127.0999292],
+    popupContent: "꾸지뽕시대 (김치찌개)",
+  },
+  {
+    latlng: [35.8036239, 127.1003873],
+    popupContent: "장어마을 (장어덮밥)",
+  },
+  {
+    latlng: [35.8034782, 127.1004966],
+    popupContent: "삼광 (백반)",
+  },
+  {
+    latlng: [35.806917, 127.1181982],
+    popupContent: "조선옥 (순댓국)",
+  },
+  {
+    latlng: [35.8171713, 127.1233623],
+    popupContent: "유성식당 (순댓국)",
+  },
+  {
+    latlng: [35.7984224, 127.1100286],
+    popupContent: "호순이감자탕 (감자탕)",
+  },
+  {
+    latlng: [35.815569, 127.1205029],
+    popupContent: "꼴통갈매기 (고기)",
+  },
+  {
+    latlng: [35.8212193, 127.0967014],
+    popupContent: "효자동그집 (중식당)",
+  },
+];
+
+markers.forEach(function (markerInfo) {
+  var marker = L.marker(markerInfo.latlng, { icon: customIcon }).addTo(map);
+  marker.bindPopup(markerInfo.popupContent);
+});
+
+map.on("click", function (e) {
+  updateInfoPanel(e.latlng);
+});
+
+function updateInfoPanel(latlng) {
+  // 클릭한 위치의 위도와 경도 업데이트
+  document.getElementById("latitude").textContent = latlng.lat.toFixed(6);
+  document.getElementById("longitude").textContent = latlng.lng.toFixed(6);
+
+  // 클릭한 위치의 주소를 지오코딩을 통해 가져오기 (OpenCage Geocoder 사용)
+  fetch(
+    "https://api.opencagedata.com/geocode/v1/json?q=" +
+      latlng.lat +
+      "+" +
+      latlng.lng +
+      "&key=YOUR_OPENCAGE_API_KEY"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const address = data.results[0].formatted;
+      document.getElementById("address").textContent = address;
+    })
+    .catch((error) => {
+      console.error("Error fetching address:", error);
+    });
+}
